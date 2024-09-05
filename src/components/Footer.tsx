060592c5222f778687d3738d48e28d98e963066a
ThanakornSaokum <thanakorn01350@gmail.com>
2024-09-05 17:23:03 +0700

    finished

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FooterProps } from "@lib/types";
 export default function Footer({ year, name, studentId }: FooterProps) {
   return (
     <div>
-      <Text> Copyright © {year} {name} {studentId} </Text>
+      <Text c="dimmed" > Copyright © {year} {name} {studentId} </Text>
     </div>
   );
 }
